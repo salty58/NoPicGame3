84fd964fb3ec875536e4334a4b8f8685f3db91a1
salty68 <lck295@nyu.edu>
2019-09-12 17:07:03 -0400

    InitCommit
    First Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float speedX = 5f;
+    float speedY = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, Time.deltaTime * speedY, 0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, Time.deltaTime * -speedY, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Time.deltaTime * -speedX, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Time.deltaTime * speedX, 0, 0);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# NoPicGame3
+# NoPicGame
+ 
